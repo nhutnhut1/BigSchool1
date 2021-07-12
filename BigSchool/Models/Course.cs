566323b5f686efa2a76e58e4cd8ab7f2410904be
nhutnhut1 <nhutp49@gmail.com>
2021-07-12 23:07:28 +0700

    BTVN va BTTL

---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,45 @@
+namespace BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+     
+        public Course()
+        {
+            Attendances = new HashSet<Attendance>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LectureId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Attendance> Attendances { get; set; }
+
+        public virtual Category Category { get; set; }
+        public List<Category> ListCategory = new List<Category>();
+
+        [Required]
+ 
+        public string LectureName;
+    }
+}
